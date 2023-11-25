b971b543135d2d150e4a56753f965bd56168d3d1
novalagung <hello@novalagung.com>
2023-11-25 18:55:51 +0700

    feat: update config

--- config/index.js
+++ config/index.js
@@ -162,7 +162,7 @@ function buildConfig({
           apiKey: algoliaApiKey,
           indexName: algoliaIndexName,
           contextualSearch: true,
-          searchPagePath: 'search', // `false`, 'search'
+          searchPagePath: 'search', // false | search
         } : undefined,
         structuredData: {
           excludedRoutes: [ ],  
